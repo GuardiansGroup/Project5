6f582eac29633e0c5ffec54a8f9f1ace23e3a253
Daniel Perez <89610746+danielperez19@users.noreply.github.com>
2021-11-29 21:55:54 -0800

    Implement new method #1

--- Driver.java
+++ Driver.java
@@ -78,6 +78,26 @@ public class Driver<T> {
 
             System.out.print(BFT2.poll() + " ");
         }
+        
+         Graph g = new Graph(13);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 3);
+        g.addEdge(0, 4);
+        g.addEdge(1, 4);
+        g.addEdge(2, 1);
+        g.addEdge(3, 6);
+        g.addEdge(4, 5);
+        g.addEdge(4, 7);
+        g.addEdge(5, 2);
+        g.addEdge(5, 7);
+        g.addEdge(6, 7);
+        g.addEdge(7, 8);
+        g.addEdge(8, 5);
+
+        
+        System.out.println("Following is the Depth First Traversal");
+        g.DFS(0);
     } // end Main
 
     public static Queue<Character> getBreadthFirstTraversal(Graph_Matrix<Character> g, int origin){
@@ -145,6 +165,76 @@ public class Driver<T> {
         }
         return bfOrder;
     } // end getBreadthFirstTraversal
+    
+    static class Graph
+    {
+        int V; //Number of Vertices
+         
+        LinkedList<Integer>[] adj; // adjacency lists
+         
+        //Constructor
+        Graph(int V)
+        {
+            this.V = V;
+            adj = new LinkedList[V];
+             
+            for (int i = 0; i < adj.length; i++)
+                adj[i] = new LinkedList<Integer>();
+             
+        }
+         
+        //To add an edge to graph
+        void addEdge(int v, int w)
+        {
+            adj[v].add(w); // Add w to v’s list.
+        }
+         
+        // prints all not yet visited vertices reachable from s
+        void DFS(int s)
+        {
+            // Initially mark all vertices as not visited
+            Vector<Boolean> visited = new Vector<Boolean>(V);
+            for (int i = 0; i < V; i++)
+                visited.add(false);
+     
+            // Create a stack for DFS
+            Stack<Integer> stack = new Stack<>();
+             
+            // Push the current source node
+            stack.push(s);
+             
+            while(stack.empty() == false)
+            {
+                // Pop a vertex from stack and print it
+                s = stack.peek();
+                stack.pop();
+                 
+                // Stack may contain same vertex twice. So
+                // we need to print the popped item only
+                // if it is not visited.
+                if(visited.get(s) == false)
+                {
+                    System.out.print(s + " ");
+                    visited.set(s, true);
+                }
+                 
+                // Get all adjacent vertices of the popped vertex s
+                // If a adjacent has not been visited, then push it
+                // to the stack.
+                Iterator<Integer> itr = adj[s].iterator();
+                 
+                while (itr.hasNext())
+                {
+                    int v = itr.next();
+                    if(!visited.get(v))
+                        stack.push(v);
+                }
+                 
+            }
+
+        }
+
+    }
 
     //public Queue<T> getDepthFirstTraversal(int origin){
         
